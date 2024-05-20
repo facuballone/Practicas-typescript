adc28a222cb1d79c047063dbe1f2cceeaa593d8b
facuballone <facuballone@gmail.com>
2024-05-20 20:20:01 -0300

    primer commit

---
+++ numbers.ts
@@ -0,0 +1,16 @@
+(()=>{
+
+let avengers:number=10;
+const villians:number=20;
+
+if(avengers<villians ){
+
+    console.log('estamos en problemas')
+
+}else{
+
+    console.log('Nos salvamos')
+}
+
+avengers= Number('55A')
+})()
